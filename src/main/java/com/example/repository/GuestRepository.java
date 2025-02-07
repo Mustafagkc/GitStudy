2177f2c33eeb3d150c87b22f61fa567c3c388688
EkremTnckr <ekremtunckir@gmail.com>
2025-02-07 13:10:12 +0300

    1.adim

---
+++ src/main/java/com/example/repository/GuestRepository.java
@@ -0,0 +1,8 @@
+package com.example.repository;
+
+import com.example.domain.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+
+}
